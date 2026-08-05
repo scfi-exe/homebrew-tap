ba78c9d0b14c14d1230c40d6486dc75cf79b9c9b
scfi-exe <scottfinkelstein97@gmail.com>
2026-08-05 06:20:28 -0400

    Update DropShot to 1.1

--- Casks/dropshot.rb
+++ Casks/dropshot.rb
@@ -1,6 +1,6 @@
 cask "dropshot" do
-  version "1.0"
-  sha256 "b21275c30e47e7f0ca68fb588f59ecc63f29e75b5f5eda07183ae6379bb34197"
+  version "1.1"
+  sha256 "9b0c02c3b6acb8f2ede0ccaefdbe9686826e40925d6dc4a9d245334817776b38"
 
   url "https://github.com/scfi-exe/DropShot/releases/download/v#{version}/DropShot-#{version}.dmg"
   name "DropShot"

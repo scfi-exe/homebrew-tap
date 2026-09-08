87f7ce0a9fab9586130d959b59651ba2e56f2bb8
scfi-exe <scottfinkelstein97@gmail.com>
2026-09-08 08:25:21 -0400

    Update DropShot to 1.2.1
    Hotfix: fixes the annotation-editor crash on Ctrl+Shift+2 → Cmd+E.

--- Casks/dropshot.rb
+++ Casks/dropshot.rb
@@ -1,6 +1,6 @@
 cask "dropshot" do
-  version "1.2"
-  sha256 "1e9f638a28c37ffd9941b21ec7979cb02a1e161e8a4f0065cdc810485ec7da0a"
+  version "1.2.1"
+  sha256 "e359537f2b32a10c1a6700b9798215fcc81fbefc30e7a919a96759ec50eb3a4c"
 
   url "https://github.com/scfi-exe/DropShot/releases/download/v#{version}/DropShot-#{version}.dmg"
   name "DropShot"
